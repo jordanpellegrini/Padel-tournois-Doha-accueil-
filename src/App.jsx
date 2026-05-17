04fc2b6395d3de943d8c77b14ed6191cf3d1d74f
jordanpellegrini <jordan.pellegrini@gmail.com>
2026-05-17 07:52:25 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import HomePage from './pages/HomePage'
 import TournamentPage from './pages/TournamentPage'
 import HistoryPage from './pages/HistoryPage'
+import RulesPage from './pages/RulesPage'
 
 export default function App() {
   return (
@@ -12,6 +13,9 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/tournament/:id" element={<TournamentPage />} />
         <Route path="/history" element={<HistoryPage />} />
+        {/* NOUVEAU : page règlement, dynamique selon les paramètres du tournoi */}
+        <Route path="/rules" element={<RulesPage />} />
+        <Route path="/rules/:id" element={<RulesPage />} />
       </Routes>
     </>
   )
